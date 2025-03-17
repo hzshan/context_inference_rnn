5c294c24588f5c46eec85da127ca0752a851497d
hzshan <hzshan@users.noreply.github.com>
2025-03-17 15:43:42 -0400

    added dynamic initialization and the task model class

--- learning.py
+++ learning.py
@@ -7,6 +7,75 @@ import inference
 # LLpT: log likelihood per trial, total_LL / n_trials
 
 
+class TaskModel:
+    def __init__(self, nc, nz, nx, d, n_epochs_each_task, w_fixate, sigma=0.01):
+        self.nc = nc
+        self.nz = nz
+        self.nx = nx
+        self.d = d
+        self.sigma = sigma
+        self.n_epochs_each_task = n_epochs_each_task
+        assert len(n_epochs_each_task) == nc
+
+        # initialize parameters
+        self.M = np.ones((nc, nz, nz))
+        self.M /= self.M.sum(axis=-1, keepdims=True)
+        self.W = np.zeros((nx, nz, d))
+        self.p0_z = np.ones(nz)
+        self.p0_z /= self.p0_z.sum()
+
+        # initialize sufficient statistics
+        self.transition_counts = np.zeros((nc, nz, nz))
+        self.W_numer = np.zeros((nx, nz, d))
+        self.W_denom = np.zeros((nx, nz))
+
+        # other artifacts for dynamic initialization
+        self.familiar_cx = np.zeros((nc, nx))
+        self.familiar_xz = np.zeros((nx, nz))
+        self.w_fixate = w_fixate.reshape(1, -1)
+        assert w_fixate.shape == (1, d)
+
+    def dynamic_initialize_W(self, trial):
+        self.W, self.familiar_cx, self.familiar_xz = dynamic_initialize_W(
+            trial=trial,
+            curr_W=self.W,
+            num_epochs_each_task=self.n_epochs_each_task,
+            familiar_cx=self.familiar_cx,
+            familiar_xz=self.familiar_xz,
+            w_fixate=self.w_fixate,
+            match_criterion=0.5)
+        
+    
+    def learn_single_trial(self, trial, lr=0.1, suff_stats_discount=0.99, iters_per_trial=10):
+        self.M, self.W, self.p0_z, self.W_numer, self.W_denom, self.transition_counts = \
+            _online_learning_single_trial(
+                curr_W=self.W,
+                curr_M=self.M,
+                curr_p0_z=self.p0_z,
+                curr_W_numer=self.W_numer,
+                curr_W_denom=self.W_denom,
+                curr_transition_counts=self.transition_counts,
+                trial=trial,
+                lr=lr,
+                suff_stats_discount=suff_stats_discount,
+                iters_per_trial=iters_per_trial,
+                sigma=self.sigma,
+                x_oracle=False,
+                eps=1e-10,
+                w_update_threshold=1e-3)
+        
+    
+    def get_stats(self, trial):
+        gamma, _, _, LL = get_stats_for_single_trial(
+            single_trial=trial,
+            W=self.W,
+            M=self.M,
+            p0_z=self.p0_z,
+            x_oracle=False,
+            sigma=self.sigma)
+        return gamma, LL
+
+
 def _update(orig, new, lr, mask=None):
     if mask is None:
         return orig * (1 - lr) + new * lr
@@ -107,7 +176,150 @@ def offline_learning(init_M,
     return learned_M, learned_W, learned_p0_z, LLpT_over_time, gamma_across_trials
 
 
-def online_learning(init_M,
+def _check_for_matching_w(W, w, match_criterion=0.5):
+    """
+    For a given vector w, check if there is a matching row in W.
+    If so, return the indices of the matching rows. 
+
+    Indices are organized as a tuple of arrays. E.g., if W is 3D and 
+    both W[3, 5, :] and W[2, 4 , :] match with w, it would return
+    (array([3, 2]), array([5, 4])).
+
+    A "match" is defined as a row in W that is within match_criterion of w in
+    terms of Euclidean distance.
+    """
+    # assert W.shape[-1] == len(w)
+    dist = np.linalg.norm(W - w, axis=-1)
+
+    matches = np.where(dist < match_criterion)
+    return matches
+
+
+def _get_ordered_clusters(obs: np.array, n_clusters: int):
+    """
+    For a given sequence of observations (T x D), do k-means clustering and
+    and order the clusters by first appearance in the sequence.
+    """
+    kmeans = sklearn.cluster.KMeans(
+        n_clusters=n_clusters,
+        random_state=0).fit(obs)
+
+    seen = set()
+    ordered_clusters = []
+    for l in kmeans.labels_:
+        if l not in seen:
+            seen.add(l)
+            ordered_clusters.append(kmeans.cluster_centers_[l])
+    return np.array(ordered_clusters)
+
+
+def dynamic_initialize_W(trial,
+                         curr_W,
+                         num_epochs_each_task: np.array,
+                         familiar_cx: np.array,
+                         familiar_xz: np.array,
+                         w_fixate,
+                         match_criterion=0.5):
+    """
+    Initialize W (parameters of the observation models) dynamically for each
+    trial.
+
+    Args:
+        trial: tuple (c, x, sy)
+        curr_W: current W; shape (nx, nz, d)
+        num_epochs_each_task: list of number of epochs for each task
+        familiar_cx: p(c, x) indicating if a (c, x) pair has been seen before
+        familiar_xz: p(x, z) indicating if a (x, z) pair has been seen before
+        w_fixate: the fixation/delay epoch for the current task
+        match_criterion: distance threshold for matching clusters
+    """
+    
+    # get some dimensions
+    nx, nz, d = curr_W.shape
+    nc = len(num_epochs_each_task)
+    assert familiar_cx.shape == (nc, nx)
+    assert familiar_xz.shape == (nx, nz)
+    
+    # hard code the 0th epoch as the F/D epoch
+    curr_W[:, 0, :] = w_fixate
+    familiar_xz[:, 0] = 1
+
+    c, _, sy = trial
+    obs = np.concatenate([sy['s'], sy['y']], axis=1)
+
+    # do k-means clustering; sort the clusters in order of first appearance.
+    # Assuming all epochs have different clusters.
+    ordered_clusters = _get_ordered_clusters(obs, num_epochs_each_task[c])
+
+    # remove clusters matching the fixation/delay epoch
+    dist_from_w_fixate = np.linalg.norm(ordered_clusters - w_fixate, axis=-1)
+    
+    non_fixate_clusters = ordered_clusters[dist_from_w_fixate > match_criterion]
+
+    # find the putative x for this trial    
+    putative_x = None
+
+    # for each x, how many clusters can be explained by it (under various z)?
+    # "explained" as in the cluster matches the w_{x,z} for some z.
+    n_clusters_matched_to_this_x = np.zeros(nx)
+
+    for w in non_fixate_clusters:
+        matched_xs, matched_zs = _check_for_matching_w(
+            curr_W, w, match_criterion)
+        n_clusters_matched_to_this_x[matched_xs] += 1
+
+    all_matched = False
+    # there exists an x that can explain all clusters
+    if np.max(n_clusters_matched_to_this_x) == len(non_fixate_clusters):
+        putative_x = np.where(n_clusters_matched_to_this_x == len(non_fixate_clusters))[0][0]
+        all_matched = True
+
+    # some of the clusters have matches. Use these to figure out the putative x.
+    # To be a possible candidate putative x, it must have some matches and also
+    # not seen before.
+    elif np.max(n_clusters_matched_to_this_x) > 0:
+         
+        candidate_putative_x = (n_clusters_matched_to_this_x == np.max(n_clusters_matched_to_this_x))
+        candidate_putative_x *= (familiar_cx[c] == 0)
+        putative_x = np.where(candidate_putative_x)[0][0]
+
+    # none of the clusters matched anything in W. In which case we arbitrarily
+    # assign an unseen x
+    else:
+        putative_x = np.where(familiar_cx[c] == 0)[0][0]
+
+    # for the assigned putative x, list the unexplained clusters
+    if not all_matched:
+        unmatched_clusters = []
+        for center in non_fixate_clusters:
+            (matched_zs, ) = _check_for_matching_w(
+                curr_W[putative_x], center, match_criterion)
+            if len(matched_zs) == 0:
+                unmatched_clusters.append(center)
+
+        
+        if len(unmatched_clusters) == 0:
+            pass
+        else:
+
+            # assign the unmatched clusters to the next available z slots for 
+            # the putative x
+            available_z_slots = np.where(familiar_xz[putative_x] == 0)[0]
+
+            assert len(available_z_slots) >= len(unmatched_clusters)
+            for j, w in enumerate(unmatched_clusters):
+                curr_W[putative_x, available_z_slots[j]] = w
+                familiar_xz[putative_x, available_z_slots[j]] = 1
+
+            # mark this (c, x) pair as familiar
+            familiar_cx[c, putative_x] = 1
+
+        
+    curr_W *= familiar_xz[:, :, None]
+    return curr_W, familiar_cx, familiar_xz
+
+
+def online_learning(init_M, 
                     init_W,
                     init_p0_z,
                     trials,
@@ -131,7 +343,9 @@ def online_learning(init_M,
         suff_stats_discount: discount factor for sufficient statistics
         iters_per_trial: number of iterations per trial
         eps: small constant for numerical stability
-        w_update_threshold: only update w params if the total expected visits of this state in the current trial exceeds this value; prevents decay of w params on rarely visited states
+        w_update_threshold: only update w params if the total expected visits of
+          this state in the current trial exceeds this value; prevents decay of
+          w params on rarely visited states
     
     Returns:
         learned_M: learned transition matrix p(z_t=i, z_{t+1}=j|c): shape (nc, nz, nz)
