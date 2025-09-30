1c7883bb7f23d68b4e1fb0fc4a089eeb824f98ca
hzshan <hzshan@users.noreply.github.com>
2025-09-30 11:04:54 -0400

    added demos for example trials and learning/inference in the task model

--- plots.py
+++ plots.py
@@ -15,3 +15,22 @@ def plot_sy(sy, boundaries=None):
         axes[i].set_ylabel(labels[i])
     axes[0].legend()
     plt.xlabel('timesteps')
+
+
+def set_rcparams():
+    plt.rcParams['figure.figsize'] = [2, 1.5]
+    plt.rcParams['figure.dpi'] = 300
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.rcParams['font.sans-serif'] = ['Helvetica Neue', 'Arial', 'sans-serif']
+    plt.rcParams['lines.linewidth'] = 0.5
+    plt.rcParams['axes.linewidth'] = 0.25
+    plt.rcParams['xtick.major.width'] = 0.25
+    plt.rcParams['ytick.major.width'] = 0.25
+    plt.rcParams['xtick.minor.width'] = 0.25
+    plt.rcParams['ytick.minor.width'] = 0.25
+    plt.rcParams['grid.linewidth'] = 0.25
+    plt.rcParams['font.size'] = 8
+    plt.rcParams['xtick.labelsize'] = 5
+    plt.rcParams['ytick.labelsize'] = 5
+    plt.rcParams['axes.labelsize'] = 8
+    plt.rcParams['legend.fontsize'] = 8
